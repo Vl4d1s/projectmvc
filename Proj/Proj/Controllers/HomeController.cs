1d40856c20070ef3c30af4dd0d3232e55acc8493
Vl4d1s <vladismarkin@gmail.com>
2020-01-02 14:41:31 +0200

    update

---
+++ Proj/Proj/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proj.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.CurrentTime1 = DateTime.Now.ToString();
+            return RedirectToAction("ShowHomePage", "Home");
+            //return View();
+        }
+
+        public ActionResult ShowHomePage()
+        {
+            //ViewData["CurrentTime"] = DateTime.Now.ToString();
+            return View();
+        }
+
+
+    }
+}
